94d2a47e1bed999b9e4b920a670a74b13238b7ca
T-Tciv <tciv@ngs.ru>
2019-06-17 17:40:24 +0600

    Внесены изменения (в соответвствии с письмом) в задачу 'Range'

---
+++ Range/src/ru/tcivinskaya/main/Main.java
@@ -0,0 +1,25 @@
+package ru.tcivinskaya.main;
+
+import ru.tcivinskaya.range.Range;
+
+public class Main {
+    public static void main(String[] args) {
+        double firstNumber = 1;
+        double lastNumber = 2.5;
+
+        Range range = new Range(firstNumber, lastNumber);
+
+        System.out.printf("Длина интервала от %f до %f равна %f.%n", range.getFrom(), range.getTo(), range.getLength());
+
+        range.setFrom(-0.5);
+        range.setTo(2);
+
+        double userNumber = 0;
+
+        if (range.isInside(userNumber)) {
+            System.out.printf("Число %f принадлежит диапазону от %f до %f.", userNumber, range.getFrom(), range.getTo());
+        } else {
+            System.out.printf("Число %f не принадлежит диапазону от %f до %f.", userNumber, range.getFrom(), range.getTo());
+        }
+    }
+}
--- Range/src/ru/tcivinskaya/range/Range.java
+++ Range/src/ru/tcivinskaya/range/Range.java
@@ -1,6 +1,6 @@
 package ru.tcivinskaya.range;
 
-class Range {
+public class Range {
     private double from;
     private double to;
 
@@ -9,29 +9,27 @@ class Range {
         this.to = to;
     }
 
+    public double getFrom() {
+        return from;
+    }
+
+    public double getTo() {
+        return to;
+    }
+
+    public void setFrom(double from) {
+        this.from = from;
+    }
+
+    public void setTo(double to) {
+        this.to = to;
+    }
+
     public double getLength() {
-        return Math.abs(to - from);
+        return to - from;
     }
 
     public boolean isInside(double userNumber) {
-        double measurementError = 1.0e-10;
-        return (((userNumber - from >= -measurementError) && (to - userNumber >= -measurementError)) || ((userNumber - to >= -measurementError) && (from - userNumber >= -measurementError)));
-    }
-
-    public static void main(String[] args) {
-        double firstNumber = 1;
-        double lastNumber = 2.5;
-
-        Range range = new Range(firstNumber, lastNumber);
-
-        System.out.printf("Длина интервала от %f до %f равна %f.%n", firstNumber, lastNumber, range.getLength());
-
-        double userNumber = 0;
-
-        if (range.isInside(userNumber)) {
-            System.out.printf("Число %f принадлежит диапазону от %f до %f.", userNumber, firstNumber, lastNumber);
-        } else {
-            System.out.printf("Число %f не принадлежит диапазону от %f до %f.", userNumber, firstNumber, lastNumber);
-        }
+        return ((userNumber >= from) && (to >= userNumber));
     }
 }
