ae2ecfb2d599ecbf130a729af903f3bf938e9772
T-Tciv <tciv@ngs.ru>
2019-06-20 00:15:49 +0600

    Задача 'Range*'

--- Range/src/ru/tcivinskaya/main/Main.java
+++ Range/src/ru/tcivinskaya/main/Main.java
@@ -5,21 +5,21 @@ import ru.tcivinskaya.range.Range;
 public class Main {
     public static void main(String[] args) {
         double firstNumber = 1;
-        double lastNumber = 2.5;
+        double lastNumber = 2;
 
         Range range = new Range(firstNumber, lastNumber);
 
         System.out.printf("Длина интервала от %f до %f равна %f.%n", range.getFrom(), range.getTo(), range.getLength());
 
-        range.setFrom(-0.5);
-        range.setTo(2);
+        range.setFrom(2);
+        range.setTo(3);
 
         double userNumber = 0;
 
         if (range.isInside(userNumber)) {
-            System.out.printf("Число %f принадлежит диапазону от %f до %f.", userNumber, range.getFrom(), range.getTo());
+            System.out.printf("Число %f принадлежит диапазону от %f до %f.%n", userNumber, range.getFrom(), range.getTo());
         } else {
-            System.out.printf("Число %f не принадлежит диапазону от %f до %f.", userNumber, range.getFrom(), range.getTo());
+            System.out.printf("Число %f не принадлежит диапазону от %f до %f.%n", userNumber, range.getFrom(), range.getTo());
         }
     }
 }
--- Range/src/ru/tcivinskaya/range/Range.java
+++ Range/src/ru/tcivinskaya/range/Range.java
@@ -32,4 +32,63 @@ public class Range {
     public boolean isInside(double userNumber) {
         return ((userNumber >= from) && (to >= userNumber));
     }
+
+    public static double getRangeLength(Range range) {
+        return range.getTo() - range.getFrom();
+    }
+
+    public static Range getIntersection(Range firstRange, Range secondRange) {
+        double firstFrom = firstRange.getFrom();
+        double firstTo = firstRange.getTo();
+        double secondFrom = secondRange.getFrom();
+        double secondTo = secondRange.getTo();
+
+        if ((firstTo < secondFrom) || (secondTo < firstFrom)) {
+            return null;
+        }
+
+        double from = (firstFrom > secondFrom) ? firstFrom : secondFrom;
+        double to = (firstTo < secondTo) ? firstTo : secondTo;
+
+        return new Range(from, to);
+    }
+
+    public static Range[] getUnion(Range firstRange, Range secondRange) {
+        double firstFrom = firstRange.getFrom();
+        double firstTo = firstRange.getTo();
+        double secondFrom = secondRange.getFrom();
+        double secondTo = secondRange.getTo();
+
+        if ((firstTo < secondFrom) || (secondTo < firstFrom)) {
+            return new Range[]{firstRange, secondRange};
+        }
+
+        double from = (firstFrom < secondFrom) ? firstFrom : secondFrom;
+        double to = (firstTo > secondTo) ? firstTo : secondTo;
+
+        return new Range[]{new Range(from, to)};
+    }
+
+    public static Range[] getDifference(Range firstRange, Range secondRange) {
+        double firstFrom = firstRange.getFrom();
+        double firstTo = firstRange.getTo();
+        double secondFrom = secondRange.getFrom();
+        double secondTo = secondRange.getTo();
+
+        if ((firstTo < secondFrom) || (secondTo < firstFrom)) {
+            return new Range[]{firstRange};
+        }
+
+        if (firstFrom < secondFrom) {
+            if (firstTo <= secondTo) {
+                return new Range[]{new Range(firstFrom, secondFrom)};
+            } else {
+                return new Range[]{new Range(firstFrom, secondFrom), new Range(secondTo, firstTo)};
+            }
+        } else if (firstTo > secondTo) {
+            return new Range[]{new Range(secondTo, firstTo)};
+        }
+
+        return new Range[]{};
+    }
 }
