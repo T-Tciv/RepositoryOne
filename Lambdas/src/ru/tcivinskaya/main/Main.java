126e6e4b71a8d59a929a2f585f07c64cf9ec6a45
T-Tciv <tciv@ngs.ru>
2019-09-09 17:12:51 +0600

    Внесены изменения (в соответствии с письмом) в задачу 'Лямбды'

--- Lambdas/src/ru/tcivinskaya/main/Main.java
+++ Lambdas/src/ru/tcivinskaya/main/Main.java
@@ -18,24 +18,37 @@ public class Main {
         LinkedList<Person> people = new LinkedList<>(Arrays.asList(person, person1, person2, person3, person4, person5, person6));
 
         //Получение списка уникальных имён
-        List<String> namesWithoutRepetitions = people.stream().map(Person::getName).distinct().collect(Collectors.toList());
+        List<String> namesWithoutRepetitions = people.stream()
+                .map(Person::getName)
+                .distinct()
+                .collect(Collectors.toList());
 
         System.out.println("Вывод списка уникальных имён:");
-        System.out.println(namesWithoutRepetitions.stream().collect(Collectors.joining(", ", "Имена: ", ".")));
+        System.out.println(namesWithoutRepetitions.stream()
+                .collect(Collectors.joining(", ", "Имена: ", ".")));
         System.out.println();
 
         //Получение списка людей младше 18
-        List<Person> youngPeople = people.stream().filter(x -> x.getAge() < 18).collect(Collectors.toList());
+        List<Person> youngPeople = people.stream()
+                .filter(x -> x.getAge() < 18)
+                .collect(Collectors.toList());
 
         System.out.println("Получение среднего возраста людей младше 18:");
-        System.out.println(youngPeople.stream().mapToInt(Person::getAge).sum() / (double) youngPeople.size());
+        System.out.println(youngPeople.stream()
+                .mapToInt(Person::getAge)
+                .average()
+                .orElseThrow(IllegalStateException::new));
         System.out.println();
 
         System.out.println("Получение Map, в котором ключи - имена, значения - средний возраст:");
-        System.out.println(people.stream().collect(Collectors.groupingBy(Person::getName, Collectors.averagingInt(Person::getAge))));
+        System.out.println(people.stream()
+                .collect(Collectors.groupingBy(Person::getName, Collectors.averagingInt(Person::getAge))));
         System.out.println();
 
         System.out.println("Вывод имён людей, чей возраст варьируется от 20 до 45, в порядке убывания возраста:");
-        people.stream().filter(p -> p.getAge() <= 45 && p.getAge() >= 20).sorted((p1, p2) -> p2.getAge() - p1.getAge()).forEach(p -> System.out.println(p.getName()));
+        people.stream()
+                .filter(p -> p.getAge() <= 45 && p.getAge() >= 20)
+                .sorted((p1, p2) -> p2.getAge() - p1.getAge())
+                .forEach(p -> System.out.println(p.getName()));
     }
 }
